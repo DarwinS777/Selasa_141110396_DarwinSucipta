550a7d2ec0670095acded57609c10a7b0934aa75
DarwinS777 <skydragon123.sd@gmail.com>
2016-11-05 23:55:49 +0700

    Latihan_5_1

--- Selasa_141110396_DarwinSucipta/Latihan_5_1/FormEditor.Designer.cs
+++ Selasa_141110396_DarwinSucipta/Latihan_5_1/FormEditor.Designer.cs
@@ -28,17 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("BgColor");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Theme", new System.Windows.Forms.TreeNode[] {
+            treeNode1});
             this.treeView1 = new System.Windows.Forms.TreeView();
             this.OK = new System.Windows.Forms.Button();
             this.Cancel = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.CBBgColor = new System.Windows.Forms.ComboBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
             // 
             // treeView1
             // 
             this.treeView1.Location = new System.Drawing.Point(12, 12);
             this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Node1";
+            treeNode1.Text = "BgColor";
+            treeNode2.Name = "Node0";
+            treeNode2.Text = "Theme";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode2});
             this.treeView1.Size = new System.Drawing.Size(190, 263);
             this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
             // 
             // OK
             // 
@@ -48,6 +65,7 @@
             this.OK.TabIndex = 1;
             this.OK.Text = "OK";
             this.OK.UseVisualStyleBackColor = true;
+            this.OK.Click += new System.EventHandler(this.OK_Click);
             // 
             // Cancel
             // 
@@ -57,17 +75,73 @@
             this.Cancel.TabIndex = 2;
             this.Cancel.Text = "Cancel";
             this.Cancel.UseVisualStyleBackColor = true;
+            this.Cancel.Click += new System.EventHandler(this.Cancel_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(208, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(269, 263);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Background Color";
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.CBBgColor);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(261, 237);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(261, 237);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // CBBgColor
+            // 
+            this.CBBgColor.FormattingEnabled = true;
+            this.CBBgColor.Location = new System.Drawing.Point(120, 12);
+            this.CBBgColor.Name = "CBBgColor";
+            this.CBBgColor.Size = new System.Drawing.Size(121, 21);
+            this.CBBgColor.TabIndex = 1;
             // 
             // FormEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(489, 287);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.Cancel);
             this.Controls.Add(this.OK);
             this.Controls.Add(this.treeView1);
             this.Name = "FormEditor";
             this.Text = "FormEditor";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormEditor_FormClosed);
+            this.Load += new System.EventHandler(this.FormEditor_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -77,5 +151,10 @@
         private System.Windows.Forms.TreeView treeView1;
         private System.Windows.Forms.Button OK;
         private System.Windows.Forms.Button Cancel;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ComboBox CBBgColor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
--- Selasa_141110396_DarwinSucipta/Latihan_5_1/FormEditor.cs
+++ Selasa_141110396_DarwinSucipta/Latihan_5_1/FormEditor.cs
@@ -19,6 +19,75 @@ namespace Latihan_5_1
         public FormEditor()
         {
             InitializeComponent();
+            Color colour = new Color();
+            PropertyInfo[] p = colour.GetType().GetProperties();
+
+            CBBgColor.DrawMode = DrawMode.OwnerDrawFixed;
+
+            foreach (PropertyInfo c in p)
+            {
+                if (c.PropertyType == typeof(System.Drawing.Color))
+                {
+                    CBBgColor.Items.Add(c.Name);
+                }
+            }
+
+            this.CBBgColor.DrawItem += new DrawItemEventHandler(CBBgColor_DrawItem);
+        }
+
+        private void CBBgColor_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            if (e.Index >= 0)
+            {
+                Graphics g = e.Graphics;
+                Brush brush = new SolidBrush(e.BackColor);
+                Brush tBrush = new SolidBrush(e.ForeColor);
+
+                g.FillRectangle(brush, e.Bounds);
+                string s = (string)this.CBBgColor.Items[e.Index].ToString();
+                SolidBrush b = new SolidBrush(Color.FromName(s));
+                e.Graphics.DrawRectangle(Pens.Black, 2, e.Bounds.Top + 1, 20, 11);
+                e.Graphics.FillRectangle(b, 3, e.Bounds.Top + 2, 19, 10);
+                e.Graphics.DrawString(s, this.Font, Brushes.Black, 25, e.Bounds.Top);
+                brush.Dispose();
+                tBrush.Dispose();
+            }
+            e.DrawFocusRectangle();
+        }
+
+        private void FormEditor_Load(object sender, EventArgs e)
+        {
+            CBBgColor.Text = form1.RichTextBoxBackColor;
+
+            treeView1.ExpandAll();
+            treeView1.SelectedNode = treeView1.Nodes[0].Nodes[0];
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (treeView1.SelectedNode == treeView1.Nodes[0].Nodes[0])
+            {
+                tabControl1.SelectedIndex = 0;
+            }
+        }
+
+        private void OK_Click(object sender, EventArgs e)
+        {
+            form1.RichTextBoxBackColor = CBBgColor.Text;
+            this.Dispose();
+            form1.ShowRichTextBox();
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+            form1.ShowRichTextBox();
+        }
+
+        private void FormEditor_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.Dispose();
+            form1.ShowRichTextBox();
         }
     }
 }
